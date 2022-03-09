37d1e1601318f4979e085f1a617e3963dd700fb9
gilangsetyawan9098 <gilangsetyawan3432@gmail.com>
2022-03-09 21:15:30 +0700

    membuat ListKomputerController

--- app/Http/Controllers/ListKomputerController.php
+++ app/Http/Controllers/ListKomputerController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\ListKomputer;
 use Illuminate\Http\Request;
 
 class ListKomputerController extends Controller
 {
     //
+    public function ListKomputer(){
+        $data = ListKomputer::all();
+        return view ('ListKomputer',['daftar'=>$data])
+        ->with('title','List');
+    }
 }
